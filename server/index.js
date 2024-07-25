3e14189ab37211a27c4badfed82010336f281c23
srinivasan-ad <adityasrinivasan09@gmail.com>
2024-07-25 09:13:12 +0530

    added connectivity to psql

--- server/index.js
+++ server/index.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const app = express();
-const cors = require('cors');
-const pool = require('./db');
+const cors = require("cors");
+const pool = require("./db");
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 app.listen("5000", () => {
   console.log("server has started on port 5000");
 });
-app.post('/login',async (req,res)=>{
+app.post("/login", async (req, res) => {
   try {
-const {name,password} = req.body;
-const newUser = await pool.query(
-  "INSERT INTO studentusers (name,password) VALUES($1,$2)",
-  [name,password]
-);
-res.json(newUser.rows[0]);
+    const { name, password } = req.body;
+    const newUser = await pool.query(
+      "INSERT INTO studentusers (name,password) VALUES($1,$2)",
+      [name, password]
+    );
+    res.json(newUser.rows[0]);
   } catch (err) {
-    console.error(err.message)
+    console.error(err.message);
   }
-})
+});
