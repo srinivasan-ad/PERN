2efa3124604e3c3dd7e2d4769e58f735d208b16a
srinivasan-ad <adityasrinivasan09@gmail.com>
2024-07-27 21:25:33 +0530

    Create user added

--- client/shopping/app/page.js
+++ client/shopping/app/page.js
@@ -1,4 +1,4 @@
-import Image from "next/image";
+'use client'
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 
--- server/index.js
+++ server/index.js
@@ -2,20 +2,29 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
+
 app.use(cors());
 app.use(express.json());
-app.listen("5000", () => {
+
+app.listen(5000, () => {
   console.log("server has started on port 5000");
 });
+
 app.post("/login", async (req, res) => {
   try {
-    const { name, password } = req.body;
+    const { username, password } = req.body;
     const newUser = await pool.query(
-      "INSERT INTO studentusers (name,password) VALUES($1,$2)",
-      [name, password]
+      "INSERT INTO studentusers (name, password) VALUES($1, $2) RETURNING *",
+      [username, password]
     );
     res.json(newUser.rows[0]);
   } catch (err) {
-    console.error(err.message);
+    if (err.code === '23505') { 
+      console.error(err.message);
+      res.status(409).json({ error: 'User already exists' });
+    } else {
+      console.log('Server error: ' + err);
+      res.status(500).send("Server Error");
+    }
   }
 });
